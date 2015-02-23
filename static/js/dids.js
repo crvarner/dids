a5c18c83f7df360409c1f1303513f939628246ce
Cole Varner <colevarn@gmail.com>
2015-02-23 12:26:42 -0800

    added remove element button to did editor
    cleaned up some thangs in dids.js
    added more comments

--- static/js/dids.js
+++ static/js/dids.js
@@ -31,22 +31,35 @@ var publishDid = function(){
     elem_count = 0;
 }
 
+/* Cancels the creation of the current 'new#' did */
 var cancelNewDid = function(id_num){
     elem_count = 0;
+    new_count--;
     $('#new'+id_num).remove();
     $('#main_container').prepend('<a class="btn" id="new_btn" onclick="newDid();">new did</a>');
 }
 
+/* Adds a text element to new did being edited */
 var addText = function(){
     $('#did_form').append('<textarea class="form-text animated" id="elem'+ elem_count +'"></textarea>');
     $('#elem'+ elem_count++).autosize();
 }
 
+/* Adds an image upload field to the new did being edited */
 var addImage = function(){
-    $('#did_form').append('<div class="image-preview"></div>'
+    $('#did_form').append('<div id="img_prev'+elem_count+'"class="image-preview"></div>'
                          +'<input id="elem'+ elem_count++ +'" type="file" />' );
 }
 
+/* Removes the last element from the currently editable did */
+var rmElement = function(){
+    if (elem_count > 0){
+        $('#elem'+ --elem_count).remove();
+        $('#img_prev'+ elem_count).remove();
+    }
+}
+
+/* Generates a new editable did */
 var newDid = function(){
     ++new_count;
     var did_string = '<div class="did" id="new' + new_count + '">'
@@ -55,14 +68,10 @@ var newDid = function(){
                     +'</div>'
                     +'<a class="btn form_btn" onclick="addText()">add text</a>'
                     +'<a class="btn form_btn" onclick="addImage()">add image</a>'
-                    +'<a class="btn form_btn" onclick="publishDid();">publish</a>'
-                    +'<a class="btn form_btn" onclick="cancelNewDid('+new_count+');">cancel</a>'
+                    +'<a class="btn form_btn" onclick="rmElement()">remove element</a>'
+                    +'<a style="float: right" class="btn form_btn" onclick="publishDid();">publish</a>'
+                    +'<a style="float: right" class="btn form_btn" onclick="cancelNewDid('+new_count+');">cancel</a>'
                     +'</div>';
     $('#did_container').prepend(did_string);
     $('#new_btn').remove();
 }
-
-$(document).ready(function(){
-    $('textarea').autosize();    
-});
-$('#main_container').prepend('<a class="btn" id="new_btn" onclick="newDid();">new did</a>');
