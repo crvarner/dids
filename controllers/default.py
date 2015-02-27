30daad0a2f9ef3210e3b78d8b7616c91c2de5431
Cole Varner <colevarn@gmail.com>
2015-02-27 10:10:18 -0800

    wooo! images!!!
    site is starting to look pretty decent.
    
    things that work:
    image upload
    image display after publish
    image display for 'old'
    title display
    jQuery autosize
    
    things that don't work:
    publish with empty file input breaks
    image preview

--- controllers/default.py
+++ controllers/default.py
@@ -17,7 +17,7 @@ def index():
 
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
-    """    
+    """
     dids = db().select(db.dids.ALL, orderby=~db.dids.date_created)
     for d in dids:
         d.body = db(db.elements.did_id==d.id).select(orderby=db.elements.stack_num)
@@ -26,8 +26,12 @@ def index():
 
 @auth.requires_login()
 def create_did():
-    data = request.vars
-    author = auth.user_id
+    """
+    creates a new did based on form data uploaded via ajax
+    """
+    data = request.vars #form data
+
+    author = auth.user_id 
     date_created = datetime.datetime.utcnow()
     
     did_id = db.dids.insert(author_id = author,
@@ -37,43 +41,27 @@ def create_did():
                             spam = 0,
                             link = None)
     
-    did = DIV(P('Author: '+str(author)),
-              P('Posted: '+str(date_created)),
-              P('Title: '+str(data['did_title'])),
+    did = DIV(H4(data['did_title']),
               _style="width:100%")
     
     num_elems = (len(data) - 1)/2
     for i in range(0, num_elems):
         d = data['elem'+str(i)]
-        if data['is_img' + str(i)]:
+        if (data['is_img' + str(i)] == 'True'):
             img_id = db.image.insert(img = db.image.img.store(d.file, d.filename))
-            
             db.elements.insert(did_id = did_id,
                 stack_num = i,
                 is_image = True,
                 element_data = img_id)
+            did.append(IMG( _src=URL('download', args = db.image(img_id).img )))
         else:
-            """db.elements.insert(did_id = did_id,
+            db.elements.insert(did_id = did_id,
                 stack_num = i,
                 is_image = False,
-                element_data = d.value)
-        
-            did.append(P('Body'+str(i)+': ' + d, _style="word-break: break-word"))"""
-        
-        #for b in data['body']:
-        #db.elements.insert(did_id = did_id,
-        #               stack_num = elem_count,
-        #               is_image = b['image'],
-        #               element_data = b['value'])
-        #if (b['image']):
-            #session.flash = b['value']
-            #img = db.image.img.store(b['value'])
-            #db.image.insert(img = img)
-            #did.append(P('ITS AN IMAGE OMG'))
-        #did.append(P( 'Body' + str(elem_count) + ': ' + b['value'], _style="word-break: break-word"))
-        #elem_count += 1
-    
-    return num_elems
+                element_data = str(d))
+            did.append(P(str(d), _style="word-break: break-word"))
+    did.append(P('posted by: '+str(db.auth_user(author).email) +' on '+ str(date_created)))
+    return did
 
 def user():
     """
--- static/js/dids.js
+++ static/js/dids.js
@@ -1,14 +1,9 @@
 var new_count = 0;
 var elem_count = 0;
 
-/*function progressHandlingFunction(e){
-    if(e.lengthComputable){
-        $('progress').attr({value:e.loaded,max:e.total});
-    }
-}*/
-
 var publishDid = function(){
     $('#did_form').submit();
+    elem_count = 0;
 }
 
 /* Cancels the creation of the current 'new#' did */
@@ -21,22 +16,23 @@ var cancelNewDid = function(id_num){
 
 /* Adds a text element to new did being edited */
 var addText = function(){
-    $('#did_form').append('<textarea class="form-text animated" id="elem'+ elem_count +'" name="elem'+elem_count +'"></textarea>'
-                         +'<input name="is_img'+ elem_count +'" type="hidden" value="False" />');
+    $('#did_form').append('<textarea id="elem'+ elem_count +'" class="form-text animated" name="elem'+elem_count +'"></textarea>'
+                         +'<input id="is_img'+ elem_count +'" name="is_img'+ elem_count +'" type="hidden" value="False" />');
     $('#elem'+ elem_count++).autosize();
 }
 
 /* Adds an image upload field to the new did being edited */
 var addImage = function(){
     $('#did_form').append('<div id="img_prev'+ elem_count +'" class="image-preview"></div>'
-                         +'<input name="elem'+ elem_count +'" type="file" />'
+                         +'<input id="elem'+ elem_count +'" name="elem'+ elem_count +'" type="file" />'
                          +'<input name="is_img'+ elem_count++ +'" type="hidden" value="True" />');
 }
 
 /* Removes the last element from the currently editable did */
 var rmElement = function(){
     if (elem_count > 0){
-        $('#elem'+ --elem_count).remove();
+        $('#is_img'+ --elem_count).remove();
+        $('#elem'+ elem_count).remove();
         $('#img_prev'+ elem_count).remove();
     }
 }
@@ -46,8 +42,7 @@ var newDid = function(){
     ++new_count;
     var did_string = '<div class="did" id="new' + new_count + '">'
                     +'<form id="did_form" enctype="multipart/form-data" action="create_did" method="post">'
-                    +'<input id="did_title" class="form-title" name="did_title" type="text"/>'
-                    //+'<input type="submit">'
+                    +'<input class="form-title" name="did_title" type="text"/>'
                     +'</form>'
                     +'<a class="btn form_btn" onclick="addText()">add text</a>'
                     +'<a class="btn form_btn" onclick="addImage()">add image</a>'
@@ -69,9 +64,9 @@ var newDid = function(){
             type: 'POST',
             success: function(data){
                 $('#new'+new_count).html(data);
+                $('#main_container').prepend('<a class="btn" id="new_btn" onclick="newDid();">new did</a>');
             }
         });
-        $('#main_container').prepend('<a class="btn" id="new_btn" onclick="newDid();">new did</a>');
     });
     $('#new_btn').remove();
 }
